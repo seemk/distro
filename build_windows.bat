58b1b479e88b712f56c848b1c8153f90db2e0206
Siim Kallas <siimkallas@gmail.com>
2016-11-13 11:54:34 +0200

    Update build script.

--- build_windows.bat
+++ build_windows.bat
@@ -1,48 +1,17 @@
-rem assumptions:
-rem
-rem - ec2 windows 2012 r2 default box (eg ami-281ad849, or equivalent, in ec2, click 'Launch' and select
-rem   'Microsoft Windows Server 2012 R2 Base', from 'Quick Start')
-rem
 rem - visual studio 2015 community
 rem    https://beta.visualstudio.com/vs/community/
 rem
 rem - cmake installed at C:\Program Files (x86)\CMake\bin\cmake.exe (3.6.2-amd64)
 rem       https://cmake.org/files/v3.6/cmake-3.6.2-win64-x64.msi
 rem
-rem - msys git available at C:\Program Files\Git (git-2.9.2 64-bit)
-rem       latest: https://github.com/git-for-windows/git/releases/download/v2.10.0.windows.1/Git-2.10.0-64-bit.exe
-rem       2.9.2 64-bit: https://github.com/git-for-windows/git/releases/download/v2.9.2.windows.1/Git-2.9.2-64-bit.exe
-rem
-rem - 7zip available at C:\Program Files\7-Zip\7z.exe (7z920-x64)
-rem       7z920-x64: http://7-zip.org/a/7z920-x64.msi
-rem
-rem Not used currently, but assumed avaliable (can ignore for now, if preparing a jenkins agent):
-rem - python 3.5 is available at c:\py35-64 (python 3.5.2-amd64) (not used currently)
-rem - cygwin64 available at c:\cygwin64 (not needed currently)
-rem
 rem Target build:
 rem - windows 64 bit
-rem - cpu architecture etc on a g2.2xlarge ec2 box
-rem
-rem environment:
-rem - jenkins agent
-rem - running out of a job/workspace directory
-rem - on C: drive, eg c:\jenkins\workspace\[job name]
-rem - workspace is wiped at start of each job (this is an option in the 'git' section of a jenkins job)
-rem - we are in a directory containing this (distro-win) already cloned, by virtue of the jenkins job bringing it down
-rem - to simulate this enviornment, open a cmd, and run:
-rem
-rem     git clone --recursive https://github.com/hughperkins/distro -b distro-win torch
-rem     cd torch
 
-rem based heavily/entirely on what hiili wrote at https://github.com/torch/torch7/wiki/Windows#using-visual-studio
 setlocal
 call "%VS140COMNTOOLS%\..\..\VC\bin\amd64\vcvars64.bat"
 @echo on
 
-if "%TORCH_LUA_VERSION%" == "" (
-    SET "TORCH_LUA_VERSION=LUAJIT21"
-)
+SET "TORCH_LUA_VERSION=LUAJIT21"
 
 set "PATH=%PATH%;C:\Program Files\CMake\bin;C:\Program Files\Git\"
 rem check if git and cmake is there
@@ -83,59 +52,34 @@ copy "%BASE%\win-files\cmake.cmd" "%BASE%\install"
 if errorlevel 1 exit goto :error
 echo did copy of cmake
 
-echo "Installing common Lua packages"
-cd %THIS_DIR%extra\luafilesystem
-cmd /c luarocks make rockspecs/luafilesystem-1.6.3-1.rockspec
-if errorlevel 1 goto :error
-cd %THIS_DIR%extra\penlight
-cmd /c luarocks make
-if errorlevel 1 goto :error
-cd %THIS_DIR%extra\lua-cjson
-cmd /c luarocks make
-if errorlevel 1 goto :error
-
 echo "Installing core Torch packages"
-cd %THIS_DIR%extra\luaffifb
-cmd /c luarocks make %Base%/win-files/luaffi-scm-1.rockspec
-if errorlevel 1 goto :error
-cd %THIS_DIR%pkg\sundown
-cmd /c luarocks make rocks/sundown-scm-1.rockspec
-if errorlevel 1 goto :error
-cd %THIS_DIR%pkg\cwrap
+cd %THIS_DIR%\pkg\cwrap
 cmd /c luarocks make rocks/cwrap-scm-1.rockspec
 if errorlevel 1 goto :error
-cd %THIS_DIR%pkg\paths
+cd %THIS_DIR%\pkg\paths
 cmd /c luarocks make rocks/paths-scm-1.rockspec
 if errorlevel 1 goto :error
-cd %THIS_DIR%pkg\torch
+
+mkdir "%BASE%\install\lib"
+set "MKL_DIR=C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2017\windows"
+set "MKL_LIB_DIR=%MKL_DIR%\mkl\lib\intel64_win"
+copy "%MKL_LIB_DIR%\mkl_blas95_lp64.lib" "%BASE%\install\lib"
+copy "%MKL_LIB_DIR%\mkl_core_dll.lib" "%BASE%\install\lib"
+copy "%MKL_LIB_DIR%\mkl_intel_lp64_dll.lib" "%BASE%\install\lib"
+copy "%MKL_LIB_DIR%\mkl_lapack95_lp64.lib" "%BASE%\install\lib"
+copy "%MKL_LIB_DIR%\mkl_sequential_dll.lib" "%BASE%\install\lib"
+copy "%MKL_DIR%\redist\intel64_win\mkl\mkl_avx2.dll" "%BASE%\install"
+copy "%MKL_DIR%\redist\intel64_win\mkl\mkl_core.dll" "%BASE%\install"
+copy "%MKL_DIR%\redist\intel64_win\mkl\mkl_sequential.dll" "%BASE%\install"
+
+cd %THIS_DIR%\pkg\torch
 cmd /c luarocks make %Base%/win-files/torch-scm-1.rockspec
 if errorlevel 1 goto :error
-cd %THIS_DIR%pkg\dok
-cmd /c luarocks make rocks/dok-scm-1.rockspec
-if errorlevel 1 goto :error
-cd %THIS_DIR%exe\trepl
-cmd /c luarocks make %Base%/win-files/trepl-scm-1.rockspec
-if errorlevel 1 goto :error
-cd %THIS_DIR%pkg\sys
-cmd /c luarocks make %Base%/win-files/sys-1.1-0.rockspec
-if errorlevel 1 goto :error
-cd %THIS_DIR%pkg\xlua
+cd %THIS_DIR%\pkg\xlua
 cmd /c luarocks make xlua-1.0-0.rockspec
 if errorlevel 1 goto :error
-cd %THIS_DIR%extra\nn
+cd %THIS_DIR%\extra\nn
 cmd /c luarocks make rocks/nn-scm-1.rockspec
-if errorlevel 1 goto :error
-cd %THIS_DIR%extra\graph
-cmd /c luarocks make %Base%/win-files/graph-scm-1.rockspec
-if errorlevel 1 goto :error
-cd %THIS_DIR%extra\nngraph
-cmd /c luarocks make
-if errorlevel 1 goto :error
-cd %THIS_DIR%pkg\image
-cmd /c luarocks make image-1.1.alpha-0.rockspec
-if errorlevel 1 goto :error
-cd %THIS_DIR%pkg\optim
-cmd /c luarocks make optim-1.0.5-0.rockspec
 if errorlevel 1 goto :error
 
 popd
